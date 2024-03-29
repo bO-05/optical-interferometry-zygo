e248938e98d4c704bae0ddda86ba08d07e8270a3
G. A. 02 <blessedbyhope05@gmail.com>
2024-03-29 08:51:39 +0700

    commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Optical Interferometry: Principles and Applications
+
+Auto-generated documentation site from [Optical Interferometry Part 1: Introduction & ZYGO GPI layout](https://www.youtube.com/watch?v=l32_QbcdUiw) and [Optical Interferometry Part 2: Measuring Optics with a Zygo GPI LC](https://www.youtube.com/watch?v=n_dUgWDrYPw) 
+
+Created using [Lumentis](https://github.com/hrishioa/lumentis)
+
+`curl -fsSL https://bun.sh/install | bash # Install bun for macOS, Linux, and WSL`
+
+`bun install`
+
+`bun dev`
+
+Change things in `pages` to see the effect.
---
+++ theme.config.jsx
@@ -0,0 +1,31 @@
+export default {
+      logo: <span>Optical Interferometry: Principles and Applications - made with Lumentis</span>,
+      editLink: {
+        component: null,
+      },
+      project: {
+        link: "https://github.com/hrishioa/lumentis",
+      },
+      feedback: {
+        content: null,
+      },
+      footer: {
+        text: (
+          <>
+            Made with 🫶 by&nbsp;
+            <a href="https://twitter.com/hrishioa" target="_blank">
+              Hrishi - say hi!
+            </a>
+          </>
+        ),
+      },
+      head: (
+        <>
+          <meta property="og:title" content="Optical Interferometry: Principles and Applications" />
+          <meta property="og:description" content="This two-part video series provides an in-depth look at optical interferometry. Part 1 covers the principles behind interferometry and the layout of a ZYGO GPI LC interferometer from the 1990s. Part 2 demonstrates how to use the interferometer to accurately measure the surface shapes and optical properties of mirrors, lenses, and microscope objectives." />
+          <meta name="robots" content="noindex, nofollow" />
+          <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/HebeHH/lumentis/choose-favicon/assets/default-favicon.png" />
+        </>
+      ),
+    };
+    
